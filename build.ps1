e641cf79c24f7472c598947707a0b6c851d96d1a
Brian Lachniet <blachniet@gmail.com>
2014-02-26 12:52:48 -0500

    Revamped packaging script

---
+++ build.ps1
@@ -0,0 +1,28 @@
+﻿function Clean{
+    rm flot -Recurse -Force -ErrorAction SilentlyContinue
+    rm flot*.zip
+    rm pkg -Recurse -Force
+}
+
+function Update{
+    #nuget update -self
+
+    $source = "http://www.flotcharts.org/downloads/flot-0.8.2.zip"
+    $destination = (Join-Path (pwd) "flot.zip")
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($source, $destination)
+}
+
+function Package{
+    mkdir flot
+    & "C:\Program Files\7-Zip\7z.exe" x "flot.zip"
+
+    mkdir pkg\Content\Scripts\flot
+    cp flot\*.js pkg\Content\Scripts\flot
+    cp flot.nuspec pkg
+    nuget pack pkg\flot.nuspec
+}
+
+Clean
+Update
+Package
